47a553bc882f19bcc425e414ec017ee397e4d738
Ygrem Bekele <ygremb@gmail.com>
2021-09-17 17:45:36 +0300

    First Publich Package Example commit 1

---
+++ notebookp/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+
+setup(
+    name="notebookp",
+    version="0.0.1",
+    author="Ygreme Bekele",
+    author_email="ygremb@gmail.com",
+    description="A package to convert your Jupyter Notebook",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ygnotebooktoall/ygnotebook",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
